78cb846553c2bbe3f3db227df80b747d01da4db3
hassehulabeck <hansbudandersson@gmail.com>
2019-10-14 15:23:08 +0200

    Added a lot, really

---
+++ bridgeData.js
@@ -0,0 +1,38 @@
+var uri = "http://data.goteborg.se/BridgeService/v1.0/GetGABOpenedStatus/1bc2ce27-c0b7-4a92-bd36-86be16e1b084/2019-01-01/2019-01-31?format=json";
+var data;
+var tbody;
+
+fetch(uri)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (myJson) {
+        data = myJson;
+        prepareData();
+    })
+
+
+function prepareData() {
+    var timeStr;
+    data.forEach(function (event, index) {
+        var startPosition = event.TimeStamp.indexOf("(");
+        var endPosition = event.TimeStamp.indexOf(")");
+        var unixTime = event.TimeStamp.slice(startPosition + 1, endPosition);
+        var time = new Date(parseInt(unixTime));
+        timeStr += "<td>" + time.toDateString() + " " + time.toTimeString() + "</td>";
+        if (index % 2 == 0) {
+            render(timeStr);
+            timeStr = "";
+        }
+    });
+}
+
+function render(timeStr) {
+    var tr = document.createElement("tr");
+    tr.innerHTML = timeStr + "</tr>";
+    tbody[0].appendChild(tr);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    tbody = document.getElementsByTagName("tbody");
+})
