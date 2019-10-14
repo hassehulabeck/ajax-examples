23518193115d29fbe2d88a949e7c2ad6e93ee2da
hassehulabeck <hansbudandersson@gmail.com>
2019-10-14 12:49:05 +0200

    First

--- ajax.js
+++ ajax.js
@@ -1,15 +1,27 @@
 var xhr = new XMLHttpRequest;
 var res;
+var list = document.getElementsByTagName("ul");
 
 xhr.onreadystatechange = function () {
     console.log(xhr.readyState);
 
     if ((xhr.readyState == 4) && (xhr.status == 200)) {
         res = xhr.response;
+        render();
     }
-    console.log(res);
 }
 
 xhr.open("GET", "https://www.hulabeck.se/html/temp/products.json");
 xhr.responseType = "json";
 xhr.send();
+
+function render() {
+    res.products.forEach(function (product) {
+        var li = document.createElement("li");
+        for (let property in product) {
+            li.innerHTML += property + ": " + product[property] + "<br />";
+
+        }
+        list[0].appendChild(li);
+    })
+}
