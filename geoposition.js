72538f1d036e3c4f8e56842b065b0de5347a92e4
hassehulabeck <hansbudandersson@gmail.com>
2019-10-17 09:11:27 +0200

    Added output

--- geoposition.js
+++ geoposition.js
@@ -1,15 +1,43 @@
+var sunsetRes;
 var pos = {
     lat: 0,
     long: 0
 };
+var url = "https://api.sunrise-sunset.org/json?"
+//lat=36.7201600&lng=-4.4203400
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        pos.lat = position.coords.latitude;
-        pos.long = position.coords.longitude;
-        var posref = document.getElementById("pos");
-        posref.innerHTML = "<h1>" + pos.lat + " " + pos.long;
-    });
-} else {
-    console.log("Din enhet tillåter inte platstjänster.")
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var posref = document.getElementById("pos");
+    var sunsetSunrise = document.getElementById("sunsetSunrise");
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            pos.lat = position.coords.latitude;
+            pos.long = position.coords.longitude;
+            posref.innerHTML = "<h1>" + pos.lat + " " + pos.long;
+
+            // Hämta data från sunset/sunrise
+            url += "lat=" + pos.lat + "&lng=" + pos.long;
+            fetch(url)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (resJson) {
+                    sunsetRes = resJson;
+                    renderSunset();
+                })
+                .catch(function (error) {
+                    console.log("Error: " + error)
+                })
+        });
+    } else {
+        console.log("Din enhet tillåter inte platstjänster.")
+    }
+
+    function renderSunset() {
+        sunsetSunrise.innerHTML = `
+        <p>Solen går upp: ${sunsetRes.results.sunrise}</p>
+        <p>Solen går ned: ${sunsetRes.results.sunset}</p>
+        `;
+    }
+})
